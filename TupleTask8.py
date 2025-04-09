9ea7ba466600500a598b3c03961c488556b4814c
Oleglad <tamerlan2802@gmail.com>
2025-04-09 12:18:51 +0700

    Delete TupleTask8.py

--- TupleTask8.py
+++
@@ -1,9 +0,0 @@
-def tpl_sort(t):
-    if all(isinstance(x, int) for x in t):
-        return tuple(sorted(t))
-    else:
-        return t
-print(tpl_sort((3, 1, 4, 1, 5, 9)))
-print(tpl_sort((3, 1, 4, 'a', 5, 9)))
-print(tpl_sort((10, -2, 0)))
-print(tpl_sort((1, 2, 3, 4.5)))
